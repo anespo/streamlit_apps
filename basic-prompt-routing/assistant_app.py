96ffbc566afc2946f3795d3efd8b817faf9311df
Tanya Rai <tanya@lastmileai.dev>
2023-11-26 19:17:01 -0500

    updated key usage

--- basic-prompt-routing/assistant_app.py
+++ basic-prompt-routing/assistant_app.py
@@ -1,16 +1,12 @@
 import asyncio
 import os
-
 import openai
 import streamlit as st
-
 from aiconfig import AIConfigRuntime
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-path = os.path.dirname(__file__)
-my_file = path+'/assistant_aiconfig.json'
-config = AIConfigRuntime.load(my_file)
-
+# Streamlit Setup
+st.title("AI Teaching Assistant")
+openai_api_key = st.text_input('OpenAI API Key', type='password')
 
 # Get assistant response based on user prompt (prompt routing)
 async def assistant_response(prompt):
@@ -26,30 +22,41 @@ async def assistant_response(prompt):
 
     return(response)
 
-# Streamlit Setup
-st.title("AI Teaching Assistant")
-st.markdown("Ask a math, physics, or general question. Based on your question, an AI math prof, physics prof, or general assistant will respond.")
-st.markdown("**This is a simple demo of prompt routing - based on your question, an LLM decides which AI teacher responds.**")
+if openai_api_key: 
+    st.markdown("Ask a math, physics, or general question. Based on your question, an AI math prof, physics prof, or general assistant will respond.")
+    st.markdown("**This is a simple demo of prompt routing - based on your question, an LLM decides which AI teacher responds.**")
 
-# Chat setup
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    # AI Config Setup
+    openai.api_key = openai_api_key
+    path = os.path.dirname(__file__)
+    my_file = path+'/assistant_aiconfig.json'
+    config = AIConfigRuntime.load(my_file)
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Chat setup
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-if prompt := st.chat_input("Ask a math, physics, or general question"):
-    st.chat_message("user").markdown(prompt)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-    chat_response = asyncio.run(assistant_response(prompt))
+    if prompt := st.chat_input("Ask a math, physics, or general question"):
+        st.chat_message("user").markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"AI: {chat_response}"
+        chat_response = asyncio.run(assistant_response(prompt))
 
-    with st.chat_message("assistant"):
-        st.markdown(response)
+        response = f"AI: {chat_response}"
 
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant"):
+            st.markdown(response)
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
+
+
 
 
